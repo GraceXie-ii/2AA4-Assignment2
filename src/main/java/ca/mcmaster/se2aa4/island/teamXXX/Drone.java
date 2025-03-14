5ae5690136c819b5fd19bf285e341002d054355e
Merlin <merlinliu1591@outlook.com>
2025-03-14 01:52:50 -0400

    inital creation of all the classes in inital UML

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class Drone implements FlyMachine{
+  
+  @Override
+  public void fly() {
+
+  }
+
+  @Override
+  public void heading() {
+
+  }
+ 
+
+}
---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/FlyMachine.java
@@ -0,0 +1,10 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public interface FlyMachine {
+  
+  public void fly();
+
+  public void heading();
+
+  
+}
