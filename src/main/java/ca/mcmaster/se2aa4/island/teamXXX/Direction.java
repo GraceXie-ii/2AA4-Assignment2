51f141ae3ec33caaa98efec398c25274effc496d
Merlin <merlinliu1591@outlook.com>
2025-03-21 00:22:35 -0400

    modified Direction enum, closed issue#4, (close #8)

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
@@ -1,5 +1,28 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
 enum Direction {
-  North, South, East, West;
+  NORTH, SOUTH, EAST, WEST;
+
+  public Direction turnLeft() {
+    switch (this) {
+        case NORTH: return WEST;
+        case WEST: return SOUTH;
+        case SOUTH: return EAST;
+        case EAST: return NORTH;
+        default: throw new IllegalStateException("Unexpected value: " + this);
+    }
 }
+
+// Turning right (clockwise)
+public Direction turnRight() {
+    switch (this) {
+        case NORTH: return EAST;
+        case EAST: return SOUTH;
+        case SOUTH: return WEST;
+        case WEST: return NORTH;
+        default: throw new IllegalStateException("Unexpected value: " + this);
+    }
+}
+
+}
+
