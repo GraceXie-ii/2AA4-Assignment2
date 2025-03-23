66916fa26f7bc3080d93c4a63f9874d6f82b0774
Merlin <merlinliu1591@outlook.com>
2025-03-23 18:32:26 -0400

    initialized private variables and getStrategy method in RectangleSearch

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RectangleSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/RectangleSearch.java
@@ -2,5 +2,14 @@ package ca.mcmaster.se2aa4.island.teamXXX;
 import org.json.JSONObject;
 
 public class RectangleSearch {
+
+  private boolean scanned = false, radared = false;
+  private boolean land = true, left = false, right = false, turned_left = false, empty_check = false;
+
+  public String getStrategy(int batteryLevel, JSONObject radarResults, JSONObject scanResults) {
+    String decision = "";
+
+    return decision;
+  }
   
 }
