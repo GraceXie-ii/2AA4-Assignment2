7888f30ef3396f36f2013d29cff2498814789014
Merlin <merlinliu1591@outlook.com>
2025-03-24 16:50:56 -0400

    initialized TopLeft class, the intend of this class will get drone to top left corner of the map

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Strategies/TopLeft.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.se2aa4.island.teamXXX.Strategies;
+
+import org.json.JSONObject;
+
+public class TopLeft implements SearchAlgorithm {
+ 
+  public String getStrategy(int batteryLevel, JSONObject radarResults, JSONObject scanResults, String droneDir, boolean allFound) {
+
+    String decision = "";
+
+
+    return decision;
+  }
+
+}
