12de79d4b147d48ace6c63c8b377fc747a57b3b9
Merlin <merlinliu1591@outlook.com>
2025-03-24 17:10:53 -0400

    initial implementation of the TopLeft class, algorithm will first go to left most edge, turn right, then go to top most edge, then turn right again to make resulting direction facing east

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Strategies/TopLeft.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Strategies/TopLeft.java
@@ -3,10 +3,39 @@ package ca.mcmaster.se2aa4.island.teamXXX.Strategies;
 import org.json.JSONObject;
 
 public class TopLeft implements SearchAlgorithm {
+
+  private boolean left_most = false, top_most = false;
  
   public String getStrategy(int batteryLevel, JSONObject radarResults, JSONObject scanResults, String droneDir, boolean allFound) {
 
     String decision = "";
+
+    if (!left_most) {
+      if (!(droneDir.equals("W"))) {
+        decision = "heading right";
+      }
+      else {
+        if (radarResults.getInt("range") > 1 || radarResults.getString("found").equals("GROUND")) {
+          decision = "fly";
+        }
+        else {
+          left_most = true;
+          decision = "heading right";
+        }
+      }
+    }
+    else if (!top_most) {
+      if (radarResults.getInt("range") > 1 || radarResults.getString("found").equals("GROUND")) {
+        decision = "fly";
+      }
+      else {
+        decision = "heading right";
+      }
+    }
+    else {
+      decision = "stop";
+    }
+
 
 
     return decision;
